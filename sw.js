3299b65b32c69131706acde3e44fc74767b82f0f
Cicobot30 <jroldant93@gmail.com>
2026-06-24 16:36:59 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE = 'cico-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/admin.html',
+  '/client.html',
+  '/manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS).catch(() => {})));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.url.includes('firestore.googleapis.com') || 
+      e.request.url.includes('identitytoolkit') ||
+      e.request.url.includes('securetoken')) return;
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
